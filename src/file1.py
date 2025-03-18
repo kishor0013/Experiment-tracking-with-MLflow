69aca0351ba253b1344d5cd5b2a3078b5fe37dfb
Kishor Timilsena <kishortimilsena2@gmail.com>
2025-03-18 19:07:46 +0545

    included Autolog

--- src/file1.py
+++ src/file1.py
@@ -20,7 +20,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random
 
 max_depth = 5
 n_estimators = 10  
-
+# mlflow.autolog()  #to log all the things automatically 
 with mlflow.start_run():
     rf = RandomForestClassifier(n_estimators=n_estimators,max_depth=max_depth,random_state=42)
     rf.fit(X_train,y_train)
